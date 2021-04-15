76390c05d03a63785d03e15d366741f1bbb9d5b0
Zach Sharp <z.sharp95@gmail.com>
2021-04-15 17:45:55 -0400

    added lake volume project setup

---
+++ lake_volume/build/project/CMakeFiles/LakeVolume.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LakeVolume.dir/source/main.cpp.obj"
+  "CMakeFiles/LakeVolume.dir/source/main.cpp.obj.d"
+  "LakeVolume.exe"
+  "LakeVolume.exe.manifest"
+  "LakeVolume.pdb"
+  "libLakeVolume.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LakeVolume.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
